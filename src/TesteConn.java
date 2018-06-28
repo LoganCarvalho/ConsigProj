5f0c236306a66a8ca86b30e35dd29bcc7aa0421c
jcarvalho <jcarvalho@KGL3101008.mshome.net>
2018-06-28 14:50:56 -0300

    Teste 2 XXX

--- src/TesteConn.java
+++ src/TesteConn.java
@@ -5,7 +5,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//teste de commit
+//XXX
 public class TesteConn {
 
 	public static void main(String[] args)
